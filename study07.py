4dfc2decbb4bbc4f5139a47a6b2b2b0d82bcb1a2
laughter <laughterj@foxmail.com>
2018-07-19 15:58:52 +0800

    关于高阶函数 filter、sorted

---
+++ study07.py
@@ -0,0 +1,27 @@
+'''
+    高阶函数
+'''
+
+'''
+    filter(function,Iterable) （function 返回bool值）  
+    函数通过function依次作用于Iterable中的元素来决定是否保留该元素
+    函数返回值为 Iterator 惰性序列
+'''
+# 过滤奇数
+def is_odd(ele):
+    return ele % 2 == 0
+print(list(filter(is_odd,list(range(10)))))   # [0, 2, 4, 6, 8]
+
+
+'''
+    sorted
+'''
+
+# 对数字序列排序
+print(sorted([2,4,1,5,7,11,3]))                    # [1, 2, 3, 4, 5, 7, 11]
+print(sorted([2,4,1,5,7,11,3],reverse = True))     # [11, 7, 5, 4, 3, 2, 1]
+
+# 对字符串序列排序
+print(sorted(['jiang','zhao','qian','sun','li']))  # ['jiang', 'li', 'qian', 'sun', 'zhao']
+print(sorted(['Jack','amanda','Bob','mike']))      # ['Bob', 'Jack', 'amanda', 'mike']
+print(sorted(['Jack','amanda','Bob','mike'],key = str.lower))
